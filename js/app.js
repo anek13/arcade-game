db520f9e371d12fb49ef71412cf9aca604719d19
Anna Wróbel <anek13@gmail.com>
2018-08-26 17:42:24 +0200

    Added multiple enemies

--- js/app.js
+++ js/app.js
@@ -1,17 +1,18 @@
 
 
 // Enemies our player must avoid
-var Enemy = function() {
+var Enemy = function(x,y, speed) {
     // Variables applied to each of our instances go here,
     // we've provided one for you to get started
 
     // The image/sprite for our enemies, this uses
     // a helper we've provided to easily load images
     this.step = 101;
-    this.x = -this.step;
-    this.y = 60;
+    this.x = x;
+    this.y = y + 60;
     this.resetPos = -this.step;
     this.sprite = 'images/enemy-bug.png';
+    this.speed = speed;
 };
 
 // Update the enemy's position, required method for game
@@ -21,7 +22,7 @@ Enemy.prototype.update = function(dt) {
     // which will ensure the game runs at the same speed for
     // all computers.
     if (this.x < this.step * 5) {
-      this.x += 100 *dt;
+      this.x += this.speed *dt;
     }
     else {
       this.x = this.resetPos;
@@ -77,11 +78,16 @@ class Hero {
 // Place the player object in a variable called player
 
 const player = new Hero();
-const bug1 = new Enemy();
+const bug1 = new Enemy(-101, 0, 120);
+const bug2 = new Enemy(-101, 83, 50);
+const bug3 = new Enemy((-101 * 2,5), 83, 30);
+const bug4 = new Enemy((-101 * 4), 83, 100);
+const bug5 = new Enemy(-101, (83 * 2), 75);
+const bug6 = new Enemy((-101 * 3,5), (83 * 2), 220);
 
 const allEnemies = [];
-allEnemies.push(bug1);
-
+allEnemies.push(bug1, bug2, bug3, bug4, bug5, bug6);
+console.log(allEnemies);
 
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
