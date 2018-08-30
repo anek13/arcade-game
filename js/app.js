16a041669043cb7700626ce3e8564c2d75093c1c
Anna Wróbel <anek13@gmail.com>
2018-08-30 11:39:33 +0200

    Enemy class as ES6

--- js/app.js
+++ js/app.js
@@ -1,44 +1,36 @@
 // Enemies our player must avoid
-var Enemy = function(y) {
-    // Variables applied to each of our instances go here,
-    // we've provided one for you to get started
-
-    // The image/sprite for our enemies, this uses
-    // a helper we've provided to easily load images
+class Enemy {
+  constructor(y) {
     this.step = 101;
     this.y = y + 60;
     this.sprite = 'images/enemy-bug.png';
     this.reset();
-};
+  }
 
-// Update the enemy's position, required method for game
-// Parameter: dt, a time delta between ticks
-Enemy.prototype.update = function(dt) {
-    // You should multiply any movement by the dt parameter
-    // which will ensure the game runs at the same speed for
-    // all computers.
-    if (this.x < this.step * 5) {
-      this.x += this.speed * dt;
-    }
-    else {
-      this.x = -this.step;
-    }
-};
+  // Update the enemy's position, required method for game
+  // Parameter: dt, a time delta between ticks
+  update(dt) {
+      if (this.x < this.step * 5) {
+        this.x += this.speed * dt;
+      }
+      else {
+        this.x = -this.step;
+      }
+  }
 
-// Draw the enemy on the screen, required method for game
-Enemy.prototype.render = function() {
-    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
-};
+  // Draw the enemy on the screen, required method for game
+  render() {
+      ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+  }
 
-//Sets start position end speed of enemy
-Enemy.prototype.reset = function() {
-    this.x = Math.floor(Math.random() * 200) - 300;
-    this.speed = Math.floor(Math.random() * 200) + 20;
-};
+  //Sets start position end speed of enemy
+  reset() {
+      this.x = Math.floor(Math.random() * 200) - 300;
+      this.speed = Math.floor(Math.random() * 200) + 20;
+  }
 
-// Now write your own player class
-// This class requires an update(), render() and
-// a handleInput() method.
+}
+
 class Hero {
   constructor() {
     this.stepX = 101;
@@ -94,9 +86,9 @@ class Hero {
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
-const row1 = 0;
-const row2 = 83;
-const row3 = 2 * 83;
+const row1 = 0;               //for enemies on the first row
+const row2 = 83;              //for enemies on second row
+const row3 = 2 * 83;          //for enemies on third row
 
 const player = new Hero();
 const allEnemies = [];
